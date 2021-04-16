01209166cd0afc2f00e2b4ea32c58865560103b8
Francis Borja <fborja4400@gmail.com>
2021-04-16 00:46:16 -0400

    update routes template
    minor update to the routes template

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
+const path = require('path');
 
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
@@ -12,13 +13,27 @@ const usersData = data.users;
  * 
  */
 router.get('/', async (req, res) => {
-  
+    res.sendFile(path.resolve('static/home.html')); // temporary
 });
 
 /**
  * 
  */
 router.post('/', async (req, res) => {
+    
+});
+
+/**
+ * Route to handle user login
+ */
+router.post('/login', async (req, res) => {
+    
+});
+
+/**
+ * Route to handle user logout
+ */
+router.get('/logout', async (req, res) => {
     
 });
 
