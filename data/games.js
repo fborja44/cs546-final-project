48819a015f449c87a0a9fee27031d1b2515d543e
eblee7 <ebleeatb@gmail.com>
2021-04-17 18:35:46 -0400

    Added createGame function

--- data/games.js
+++ data/games.js
@@ -4,6 +4,16 @@ const mongoCollections = require('../config/mongoCollections');
 const games = mongoCollections.games;
 let { ObjectId } = require('mongodb');
 
+// https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+const validURL = new RegExp('^(https?:\\/\\/)?'+ // protocol
+'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+const validPrice = /^\$(\d+\.\d{1,2})$/
+
 /**
  * Creates a game in the database using the following parameters:
  * @param {string} title Title of the video game.
@@ -16,7 +26,109 @@ let { ObjectId } = require('mongodb');
  * @param {Object[]} prices An array of price subdocuments.
  */
 async function createGame(title, image, publisher, genres, releaseYear, platforms, description, prices) {
+    // title error checking
+    if (!title) throw "A title must be provided";
+    if (typeof title !== 'string') throw `${title || "provided argument"} must be a string`;
+    if (title.trim().length === 0) throw "The title must not be an empty string";
 
+    // image error checking
+    if (!image) throw "An image must be provided";
+    if (typeof image !== 'string') throw `${image || "provided argument"} must be a string`;
+    if (image.trim().length === 0) throw "The image must not be an empty string";
+    
+    if (!validURL.test(image)) throw `${image || "provided argument"} must be a valid url`;
+
+    // publisher error checking
+    if (!publisher) throw "A publisher must be provided";
+    if (typeof publisher !== 'string') throw `${publisher || "provided argument"} must be a string`;
+    if (publisher.trim().length === 0) throw "The publisher must not be an empty string";
+
+    // genres error checking
+    if (!genres) throw "A genres array must be provided";
+    if (!Array.isArray(genres)) throw `${genres || "provided argument"} must be an array`;
+    if (genres.length === 0) throw "The genres array must have at least one genre";
+    let genresTrim = [];
+    for (let x of genres) {
+        if (typeof x !== 'string') throw `${x || "provided argument"} must be a string`;
+        if (x.trim().length === 0) throw "The genre must not be an empty string";
+        genresTrim = genresTrim.append(x.trim());
+    }
+
+    // releaseYear error checking
+    if (!releaseYear) throw "A releaseYear must be provided";
+    if (typeof releaseYear !== 'string') throw `${releaseYear || "provided argument"} must be a string`;
+    if (releaseYear.trim().length === 0) throw "The releaseYear must not be an empty string";
+    if (releaseYear.trim().length !== 4) throw "The releaseYear must be a valid year";
+    const releaseYearParsed = parseInt(releaseYear);
+    if (isNaN(releaseYearParsed)) throw "The releaseYear must be a valid year";
+    let d = new Date();
+    if (releaseYearParsed < 1930 || releaseYearParsed > d.getFullYear() + 5) throw "The releaseYear must be a valid year";
+
+    // platforms error checking
+    if (!platforms) throw "A platforms array must be provided";
+    if (!Array.isArray(platforms)) throw `${platforms || "provided argument"} must be an array`;
+    if (platforms.length === 0) throw "The platforms array must have at least one platform";
+    let platformsTrim = [];
+    for (let x of platforms) {
+        if (typeof x !== 'string') throw `${x || "provided argument"} must be a string`;
+        if (x.trim().length === 0) throw "The platform must not be an empty string";
+        platformsTrim = platformsTrim.append(x.trim());
+    }
+
+    // description error checking
+    if (!description) throw "A description must be provided";
+    if (typeof description !== 'string') throw `${description || "provided argument"} must be a string`;
+    if (description.trim().length === 0) throw "The description must not be an empty string";
+
+    // prices error checking
+    if (!prices) throw "A prices array must be provided";
+    if (!Array.isArray(prices)) throw `${prices || "provided argument"} must be an array`;
+    if (prices.length === 0) throw "The prices array must have at least one price"
+    let pricesTrim = []
+    for (let x of prices) {
+        if (typeof x !== 'object') throw `${x || "provided argument"} must be an object`;
+        if (Array.isArray(x)) throw `${x || "provided argument"} must be an object`;
+        if (x === null) throw `${x || "provided argument"} must be an object`
+        if (Object.keys(x).length !== 2) throw "The price must be of the correct format";
+
+        let obj = {};
+        // price error checking
+        if (!x.price) throw "The price must be provided";
+        if (typeof x.price !== 'string') throw `${x.price || "provided argument"} must be a string`;
+        if (x.price.trim().length === 0) throw "The price must not be an empty string";
+        if (!validPrice.test(x.price)) throw `${x.price || "provided argument"} must be a valid price`;
+        obj.price = x.price.trim();
+
+        // platform error checking
+        if (!x.platform) throw "The platform must be provided";
+        if (typeof x.platform !== 'string') throw `${x.platform || "provided argument"} must be a string`;
+        if (x.platform.trim().length === 0) throw "The platform must not be an empty string";
+        obj.platform = x.platform.trim();
+
+        pricesTrim = pricesTrim.append(obj);
+    }
+
+    const gameCollection = await games();
+
+    let newGame = {
+        title: title.trim(),
+        image: image.trim(),
+        publisher: publisher.trim(),
+        genres: genresTrim,
+        releaseYear: releaseYear.trim(),
+        platforms: platformsTrim,
+        description: description,
+        prices: pricesTrim,
+        averageRating: 0,
+        reviews: [],
+    };
+
+    const insertInfo = await gameCollection.insertOne(newGame);
+    if (insertInfo.insertedCount === 0) throw "Could not create game";
+    const id = insertInfo.insertedId.toString();
+
+    const game = await this.getGameById(id);
+    return game;
 }
 
 /**
