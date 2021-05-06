f4c719dda5abc303e424845decca05ce5043bf8c
Naomi Zheng <naomizheng@Naomis-MacBook-Pro.local>
2021-05-06 11:13:36 -0400

    reviews routing and handlebar

---
+++ routes/reviews.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data/');
+const path = require('path');
+const reviewsData = data.reviews;
+const gamesData = data.games;
+
+router.post('/', async (req, res) => {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1); //January is 0!
+  var yyyy = today.getFullYear();
+  today = mm + '/' + dd + '/' + yyyy;
+
+  let reviewPost = req.body;
+  let errors = [];
+
+  let game = await gamesData.getGameByTitle(reviewPost.gameTitle);
+  if (!reviewPost.reviewTitle || reviewPost.reviewTitle.trim().length===0) {
+    errors.push('No title provided');
+      console.log("enter");
+  }
+
+  if (!reviewPost.reviewBody || reviewPost.reviewBody.trim()===0) {
+    errors.push('No body provided');
+  }
+
+  if (!reviewPost.reviewRating) {
+    errors.push('No rating provided');
+  }
+
+    if (errors.length > 0) {
+        res.render('games/single', {
+            title:game.title,
+            game:game,
+            errors: errors,
+            hasErrors: true
+        });
+        return;
+    }
+
+  try {
+    const newReview = await reviewsData.createReview(
+      game._id,
+      reviewPost.reviewTitle,
+      {username:"idk",_id: "234982374"},
+      today.toString(),
+      reviewPost.reviewBody,
+      parseInt(reviewPost.reviewRating)
+    )
+      res.redirect(`/games/${game.title}`);
+
+  }catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "can't make game" });
+  }
+});
+
+module.exports = router;
