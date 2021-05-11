804ba168febe04abc5709c63b25232cae005dbcb
bxia5 <80549714+bxia5@users.noreply.github.com>
2021-05-11 12:19:40 -0400

    Add follows and debug
    Add
    -follows
    
    debug
    -/private/edit with partial problem

--- routes/users.js
+++ routes/users.js
@@ -106,7 +106,7 @@ router.get('/private/edit', async (req, res) => {
         res.redirect('/');
         return;
     }
-    res.render('users/edit',  {signed_in: req.body.signed_in});
+    res.render('users/edit',  {signed_in: req.body.signed_in, partial:"gameForm"});
  
 });
 
@@ -305,7 +305,7 @@ router.get('/private/:id', async (req, res) => {
     try {
         const user = await usersData.getUserById(id);
 
-        res.render('users/private', {title: user.username, user: user, reviewsEmpty: user.reviews.length === 0, likesEmpty: user.likes.length === 0, followsEmpty: user.follows.length === 0, wishEmpty: user.wishlist.length === 0,signed_in: req.body.signed_in , partial:'gameForm'});
+        res.render('users/private', {title: user.username, user: user, reviewsEmpty: user.reviews.length === 0, likesEmpty: user.likes.length === 0, followsEmpty: user.follows.length === 0, wishEmpty: user.wishlist.length === 0,signed_in: req.body.signed_in , partial:'signup'});
 
 
     } catch (e) {
