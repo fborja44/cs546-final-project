beb30b52ca3b30d843ffa12b0a989155c19ef02a
bxia5 <80549714+bxia5@users.noreply.github.com>
2021-05-12 16:13:54 -0400

    review client check

---
+++ public/js/review.js
@@ -0,0 +1,40 @@
+$("#review-form").submit(function(event) {
+
+    $("#reviewList").empty();
+    $("#reviewError").empty();
+    $("#reviewError").hide();
+
+    let gameTitle = $("#reviewTitle").val().trim();
+    let gameId = $("#reviewTitle").val().trim();
+    let title = $("#reviewTitle").val().trim();
+    let rating = $("#reviewRating").val();
+    let review = $("#reviewBody").val().trim();
+
+    
+    let error = false;
+    let message = null;
+
+    if(!gameTitle || !gameId){
+        error = true;
+        message = "Emmm... it is interesting... Nothing to submit."
+    }
+
+    if (!error && (!title || !rating || !review)){
+        error = true;
+        message = "Error: Title, rating or review is missing."
+    }
+
+    if (!error && ( rating < 1 || rating > 5)){
+        error = true;
+        message = "Error: The rating should between 1 and 5."
+    }
+
+
+    if (error){
+        event.preventDefault();
+        let htmlStr = `<li class="reviewErrors">${message}</li>`;
+        $("#reviewError").append(htmlStr);
+        $("#reviewError").show();
+    }
+
+})
