4df9151f780b8cd8b5611d007fb75151f9cb5248
Francis Borja <fborja4400@gmail.com>
2021-05-14 20:56:18 -0400

    Prepare seed file for demonstration

--- tasks/seed.js
+++ tasks/seed.js
@@ -130,50 +130,50 @@ const main = async () => {
 
     /* Creating Users
     ---------------------------------------------------------------------------*/
-    let user1 = await usersData.createUser(
-        "fborja44",
-        "Francis",
-        "Borja",
-        "fborja@stevens.edu",
-        "supersecret"
-    )
-    let user1_id = user1._id.toString();
+    // let user1 = await usersData.createUser(
+    //     "fborja44",
+    //     "Francis",
+    //     "Borja",
+    //     "fborja@stevens.edu",
+    //     "supersecret"
+    // )
+    // let user1_id = user1._id.toString();
 
-    let user2 = await usersData.createUser(
-        "naomi",
-        "Naomi",
-        "Zheng",
-        "nzheng1@stevens.edu",
-        "password"
-    )
-    let user2_id = user2._id.toString();
+    // let user2 = await usersData.createUser(
+    //     "naomi",
+    //     "Naomi",
+    //     "Zheng",
+    //     "nzheng1@stevens.edu",
+    //     "password"
+    // )
+    // let user2_id = user2._id.toString();
 
-    let user3 = await usersData.createUser(
-        "brian",
-        "Brian",
-        "Lee",
-        "elee9@stevens.edu",
-        "password"
-    )
-    let user3_id = user3._id.toString();
+    // let user3 = await usersData.createUser(
+    //     "brian",
+    //     "Brian",
+    //     "Lee",
+    //     "elee9@stevens.edu",
+    //     "password"
+    // )
+    // let user3_id = user3._id.toString();
 
-    let user4 = await usersData.createUser(
-        "bingxin",
-        "Bingxin",
-        "Xia",
-        "bxia5@stevens.edu",
-        "password"
-    )
-    let user4_id = user4._id.toString();
+    // let user4 = await usersData.createUser(
+    //     "bingxin",
+    //     "Bingxin",
+    //     "Xia",
+    //     "bxia5@stevens.edu",
+    //     "password"
+    // )
+    // let user4_id = user4._id.toString();
 
-    let user5 = await usersData.createUser(
-        "BestProfessor",
-        "Patrick",
-        "Hill",
-        "phill@stevens.edu",
-        "cs546spring"
-    )
-    let user5_id = user5._id.toString();
+    // let user5 = await usersData.createUser(
+    //     "BestProfessor",
+    //     "Patrick",
+    //     "Hill",
+    //     "phill@stevens.edu",
+    //     "cs546spring"
+    // )
+    // let user5_id = user5._id.toString();
 
     /* Testing Users functions
     ---------------------------------------------------------------------------*/
@@ -196,236 +196,236 @@ const main = async () => {
 
     /* Creating reviews
     ---------------------------------------------------------------------------*/
-    /* Game 1 Reviews: Cyberpunk 2077
-    -------------------------------------*/
-    let review1_1 = await reviewsData.createReview(
-        game1_id,
-        "nice",
-        { username: user1.username, _id: user1_id },
-        "4/18/2020",
-        "pretty cool",
-        4
-    )
+    // /* Game 1 Reviews: Cyberpunk 2077
+    // -------------------------------------*/
+    // let review1_1 = await reviewsData.createReview(
+    //     game1_id,
+    //     "nice",
+    //     { username: user1.username, _id: user1_id },
+    //     "4/18/2020",
+    //     "pretty cool",
+    //     4
+    // )
     
-    let reviewUser1 = [];
-    reviewUser1.push(review1_1);
-    await usersData.addReviews(user1._id, reviewUser1)
+    // let reviewUser1 = [];
+    // reviewUser1.push(review1_1);
+    // await usersData.addReviews(user1._id, reviewUser1)
 
-    let review1_2 = await reviewsData.createReview(
-        game1_id,
-        "sucks",
-        { username: user2.username, _id: user2_id },
-        "4/15/2020",
-        "this game sucks",
-        1
-    )
+    // let review1_2 = await reviewsData.createReview(
+    //     game1_id,
+    //     "sucks",
+    //     { username: user2.username, _id: user2_id },
+    //     "4/15/2020",
+    //     "this game sucks",
+    //     1
+    // )
 
-    let reviewUser2 = [];
-    reviewUser2.push(review1_2);
-    await usersData.addReviews(user2._id, reviewUser2)
+    // let reviewUser2 = [];
+    // reviewUser2.push(review1_2);
+    // await usersData.addReviews(user2._id, reviewUser2)
 
-    let review1_3 = await reviewsData.createReview(
-        game1_id,
-        "ok i guess",
-        { username: user3.username, _id: user3_id },
-        "1/12/2021",
-        "it was ok",
-        3
-    )
+    // let review1_3 = await reviewsData.createReview(
+    //     game1_id,
+    //     "ok i guess",
+    //     { username: user3.username, _id: user3_id },
+    //     "1/12/2021",
+    //     "it was ok",
+    //     3
+    // )
 
-    let reviewUser3 = [];
-    reviewUser3.push(review1_3);
-    await usersData.addReviews(user3._id, reviewUser3)
+    // let reviewUser3 = [];
+    // reviewUser3.push(review1_3);
+    // await usersData.addReviews(user3._id, reviewUser3)
 
-    /* Game 2 Reviews: Breath of the Wild
-    -------------------------------------*/
-    let review2_1 = await reviewsData.createReview(
-        game2_id,
-        "This Game is Awesome!",
-        { username: user2.username, _id: user2_id },
-        "4/18/2017",
-        "This is the greatest game ever! Nintendo really outdid themselves",
-        5
-    )
+    // /* Game 2 Reviews: Breath of the Wild
+    // -------------------------------------*/
+    // let review2_1 = await reviewsData.createReview(
+    //     game2_id,
+    //     "This Game is Awesome!",
+    //     { username: user2.username, _id: user2_id },
+    //     "4/18/2017",
+    //     "This is the greatest game ever! Nintendo really outdid themselves",
+    //     5
+    // )
     
-    reviewUser2.push(review2_1);
-    await usersData.addReviews(user2._id, reviewUser2)
+    // reviewUser2.push(review2_1);
+    // await usersData.addReviews(user2._id, reviewUser2)
 
-    let review2_2 = await reviewsData.createReview(
-        game2_id,
-        "Could be better.",
-        { username: user4.username, _id: user4_id },
-        "6/23/2018",
-        "It was an ok game I guess.",
-        3
-    )
+    // let review2_2 = await reviewsData.createReview(
+    //     game2_id,
+    //     "Could be better.",
+    //     { username: user4.username, _id: user4_id },
+    //     "6/23/2018",
+    //     "It was an ok game I guess.",
+    //     3
+    // )
 
-    let reviewUser4 = [];
-    reviewUser4.push(review2_2);
-    await usersData.addReviews(user4._id, reviewUser4)
+    // let reviewUser4 = [];
+    // reviewUser4.push(review2_2);
+    // await usersData.addReviews(user4._id, reviewUser4)
 
-    let review2_3 = await reviewsData.createReview(
-        game2_id,
-        "5 Stars",
-        { username: user5.username, _id: user5_id },
-        "6/23/2018",
-        "^^^",
-        5
-    )
+    // let review2_3 = await reviewsData.createReview(
+    //     game2_id,
+    //     "5 Stars",
+    //     { username: user5.username, _id: user5_id },
+    //     "6/23/2018",
+    //     "^^^",
+    //     5
+    // )
     
-    let reviewUser5 = [];
-    reviewUser5.push(review2_3);
-    await usersData.addReviews(user5._id, reviewUser5)
+    // let reviewUser5 = [];
+    // reviewUser5.push(review2_3);
+    // await usersData.addReviews(user5._id, reviewUser5)
 
-    let review2_4 = await reviewsData.createReview(
-        game2_id,
-        "lol",
-        { username: user1.username, _id: user1_id },
-        "2/15/2019",
-        "this is a review",
-        5
-    )
+    // let review2_4 = await reviewsData.createReview(
+    //     game2_id,
+    //     "lol",
+    //     { username: user1.username, _id: user1_id },
+    //     "2/15/2019",
+    //     "this is a review",
+    //     5
+    // )
     
-    reviewUser1.push(review2_4);
-    await usersData.addReviews(user1._id, reviewUser1)
+    // reviewUser1.push(review2_4);
+    // await usersData.addReviews(user1._id, reviewUser1)
 
-    /* Game 3 Reviews: Valorant
-    -------------------------------------*/
-    let review3_1 = await reviewsData.createReview(
-        game3_id,
-        "this game is bad",
-        { username: user3.username, _id: user3_id },
-        "5/1/2021",
-        "its not good",
-        1
-    )
-    reviewUser3.push(review3_1);
-    await usersData.addReviews(user3._id, reviewUser3)
+    // /* Game 3 Reviews: Valorant
+    // -------------------------------------*/
+    // let review3_1 = await reviewsData.createReview(
+    //     game3_id,
+    //     "this game is bad",
+    //     { username: user3.username, _id: user3_id },
+    //     "5/1/2021",
+    //     "its not good",
+    //     1
+    // )
+    // reviewUser3.push(review3_1);
+    // await usersData.addReviews(user3._id, reviewUser3)
 
-    /* Game 4 Reviews: Emerald
-    -------------------------------------*/
-    let review4_1 = await reviewsData.createReview(
-        game4_id,
-        "title",
-        { username: user1.username, _id: user1_id },
-        "1/1/2000",
-        "dope",
-        4
-    )
-    reviewUser1.push(review4_1);
-    await usersData.addReviews(user1._id, reviewUser1)
+    // /* Game 4 Reviews: Emerald
+    // -------------------------------------*/
+    // let review4_1 = await reviewsData.createReview(
+    //     game4_id,
+    //     "title",
+    //     { username: user1.username, _id: user1_id },
+    //     "1/1/2000",
+    //     "dope",
+    //     4
+    // )
+    // reviewUser1.push(review4_1);
+    // await usersData.addReviews(user1._id, reviewUser1)
     
-    /* Game 6 Reviews: NBA 2K20
-    -----------------------------------------------------*/
-    let review6_1 = await reviewsData.createReview(
-        game6_id,
-        "kd is a snake",
-        { username: user4.username, _id: user4_id },
-        "8/23/2020",
-        "sssss",
-        2
-    )
-    reviewUser4.push(review6_1);
-    await usersData.addReviews(user4._id, reviewUser4)
+    // /* Game 6 Reviews: NBA 2K20
+    // -----------------------------------------------------*/
+    // let review6_1 = await reviewsData.createReview(
+    //     game6_id,
+    //     "kd is a snake",
+    //     { username: user4.username, _id: user4_id },
+    //     "8/23/2020",
+    //     "sssss",
+    //     2
+    // )
+    // reviewUser4.push(review6_1);
+    // await usersData.addReviews(user4._id, reviewUser4)
 
-    /* Game 8 Reviews: Hades
-    -------------------------------------*/
-    let review8_1 = await reviewsData.createReview(
-        game8_id,
-        "The Best Game Ever",
-        { username: user2.username, _id: user2_id },
-        "11/9/2020",
-        "Supergiant always makes awesome games!",
-        5
-    )
-    reviewUser2.push(review8_1);
-    await usersData.addReviews(user2._id, reviewUser2)
+    // /* Game 8 Reviews: Hades
+    // -------------------------------------*/
+    // let review8_1 = await reviewsData.createReview(
+    //     game8_id,
+    //     "The Best Game Ever",
+    //     { username: user2.username, _id: user2_id },
+    //     "11/9/2020",
+    //     "Supergiant always makes awesome games!",
+    //     5
+    // )
+    // reviewUser2.push(review8_1);
+    // await usersData.addReviews(user2._id, reviewUser2)
 
-    let review8_2 = await reviewsData.createReview(
-        game8_id,
-        "The Best Game Ever2",
-        { username: user5.username, _id: user5_id },
-        "11/9/2020",
-        "Supergiant always makes awesome games!",
-        4
-    )
-    reviewUser5.push(review8_2);
-    await usersData.addReviews(user5._id, reviewUser5)
+    // let review8_2 = await reviewsData.createReview(
+    //     game8_id,
+    //     "The Best Game Ever2",
+    //     { username: user5.username, _id: user5_id },
+    //     "11/9/2020",
+    //     "Supergiant always makes awesome games!",
+    //     4
+    // )
+    // reviewUser5.push(review8_2);
+    // await usersData.addReviews(user5._id, reviewUser5)
 
-    let review8_3 = await reviewsData.createReview(
-        game8_id,
-        "The Best Game Ever3",
-        { username: user1.username, _id: user1_id },
-        "11/9/2020",
-        "Supergiant always makes awesome games!",
-        4
-    )
-    reviewUser1.push(review8_3);
-    await usersData.addReviews(user1._id, reviewUser1)
+    // let review8_3 = await reviewsData.createReview(
+    //     game8_id,
+    //     "The Best Game Ever3",
+    //     { username: user1.username, _id: user1_id },
+    //     "11/9/2020",
+    //     "Supergiant always makes awesome games!",
+    //     4
+    // )
+    // reviewUser1.push(review8_3);
+    // await usersData.addReviews(user1._id, reviewUser1)
 
     /* Testing Games functions
     ---------------------------------------------------------------------------*/
     // Testing getAllGames()
-    let games = await gamesData.getAllGames();
+    // let games = await gamesData.getAllGames();
     // console.log(games);
 
     // Testing getGameById()
-    let test1 = await gamesData.getGameById(game1_id);
+    // let test1 = await gamesData.getGameById(game1_id);
     // console.log(test1);
 
     // Testing getAllReviews()
-    let reviews1 = await reviewsData.getAllReviews(game1_id);
+    // let reviews1 = await reviewsData.getAllReviews(game1_id);
     // console.log(reviews1);
 
     // Testing updateGameRating()
     // let update1 = await gamesData.updateGameRating(game1_id, 4.2);
 
     // Testing updateGameById()
-    let update2 = await gamesData.updateGameById(game1_id,
-        {
-            prices: [{ price: "$49.99", platform: "PC" }, { price: "$49.99", platform: "Xbox One" }, { price: "$49.99", platform: "PlayStation 5" }]
-        });
+    // let update2 = await gamesData.updateGameById(game1_id,
+    //     {
+    //         prices: [{ price: "$49.99", platform: "PC" }, { price: "$49.99", platform: "Xbox One" }, { price: "$49.99", platform: "PlayStation 5" }]
+    //     });
 
     // Testing removeGameById()
-    let delete_this = await gamesData.createGame(
-        "delete",
-        "badurl.png",
-        "no",
-        ["hi"],
-        "2001",
-        ["samsung smart fridge"],
-        "ok",
-        [{ price: "$0.00", platform: "samsung smart fridge" }]
-    )
-    let delete1_id = delete_this._id;
+    // let delete_this = await gamesData.createGame(
+    //     "delete",
+    //     "badurl.png",
+    //     "no",
+    //     ["hi"],
+    //     "2001",
+    //     ["samsung smart fridge"],
+    //     "ok",
+    //     [{ price: "$0.00", platform: "samsung smart fridge" }]
+    // )
+    // let delete1_id = delete_this._id;
 
-    let delete1 = await gamesData.removeGameById(delete1_id);
+    // let delete1 = await gamesData.removeGameById(delete1_id);
     
     // Testing getGamesByRating
-    let ratings1 = await gamesData.getGamesByRating(2);
+    // let ratings1 = await gamesData.getGamesByRating(2);
     // console.log(ratings1);
 
     // Testing getGamesByGenre
-    let genre1 = await gamesData.getGamesByGenre("action");
+    // let genre1 = await gamesData.getGamesByGenre("action");
     // console.log(genre1);
 
     // Testing getGamesByPlatform
-    let platform1 = await gamesData.getGamesByPlatform("pc");
+    // let platform1 = await gamesData.getGamesByPlatform("pc");
     // console.log("platorm1");
 
     // Testing getBestGame
-    let best1 = await gamesData.getBestGame();
+    // let best1 = await gamesData.getBestGame();
     // console.log(best1);
 
     // Testing getGamesByPrice
-    let price1 = await gamesData.getGamesByPrice("$30.00");
+    // let price1 = await gamesData.getGamesByPrice("$30.00");
     // console.log(price1);
 
     // Testing getBestGameByGenre
-    let bestgenre1 = await gamesData.getBestGameByGenre("Action");
+    // let bestgenre1 = await gamesData.getBestGameByGenre("Action");
     // console.log(bestgenre1);
 
-     /* Testing Review functions
+    /* Testing Review functions
     ---------------------------------------------------------------------------*/
     //Testing Delete Review();
     //console.log("review Testing");
